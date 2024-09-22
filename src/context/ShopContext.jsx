d617a74764239052b9d608fda6c205dd9ae1856e
G-Prasath <guruprasathmsc@gmail.com>
2024-09-22 21:32:09 +0530

    Get Cart count pending

--- src/context/ShopContext.jsx
+++ src/context/ShopContext.jsx
@@ -1,5 +1,6 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 import { products } from "../assets/assets";
+import { toast } from "react-toastify";
 
 export const ShopContext = createContext();
 
@@ -8,9 +9,43 @@ const ShopContextProvider = (props) => {
     const delivery_fee = 10;
     const [search, setSearch] = useState('');
     const [showSearch, setShowSearch] = useState(false);
+    const [cartItem, setCartItem] = useState({});
+
+    const addToCart = async (itemId, size) => {
+        let cartData = structuredClone(cartItem);
+
+        if (size) {
+            if (cartData[itemId]) {
+                if (cartData[itemId][size]) {
+                    cartData[itemId][size] += 1;
+                }
+                else {
+                    cartData[itemId][size] = 1;
+                }
+            }
+            else {
+                cartData[itemId] = {};
+                cartData[itemId][size] = 1;
+            }
+
+        }else{
+            toast.error("Select Product Size");  
+        }
+
+
+
+        setCartItem(cartData)
+    }
+
+    useEffect(() => {
+        console.log(cartItem);
+
+    }, [cartItem])
 
     const value = {
-        products, currency, delivery_fee, search, setSearch, showSearch, setShowSearch
+        products, currency, delivery_fee,
+        search, setSearch, showSearch, setShowSearch,
+        cartItem, addToCart
     }
 
     return (
