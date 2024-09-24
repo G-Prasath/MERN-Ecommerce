30f724b3879e0f538742d428cd16d0cfc09881ec
G-Prasath <guruprasathmsc@gmail.com>
2024-09-24 17:29:24 +0530

    Fortend Completed

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,11 +1,46 @@
-import React from 'react'
+import React from "react";
+import Title from "../components/Title";
+import { assets } from '../assets/assets';
+import NewLetterBox from "../components/NewLetterBox";
 
 const About = () => {
   return (
     <div>
-      About
-    </div>
-  )
-}
+      <div className="text-2xl text-center pt-8 border-t">
+        <Title title1={`About`} title2={`us`} />
+      </div>
 
-export default About
+      <div className="my-10 flex flex-col md:flex-row gap-16">
+        <img src={assets.about_img} className="w-full md:max-w-[450px]" alt="About Images" />
+        <div className="flex flex-col justify-center gap-6 md:w-2/4 text-gray-600">
+          <p className="text-justify">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Maxime veritatis perferendis velit dignissimos asperiores omnis nisi aliquam. Vero, molestias amet. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vitae, enim.</p>
+          <p className="text-justify">Lorem ipsum dolor sit amet, consectetur adipisicing elit. In fuga ad aperiam maiores laboriosam perspiciatis beatae neque eos provident. Laborum doloremque unde rerum fuga! Accusamus dicta aperiam voluptatibus repellat beatae.</p>
+          <b className="text-gray-800">Our Mission</b>
+          <p className="text-justify">Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus aliquam aliquid consequatur reiciendis quam nam quis est neque numquam dolorem veritatis, fugit ipsam amet facilis sed tempore, voluptatibus, velit iure.</p>
+        </div>
+      </div>
+
+      <div className="text-xl py-4">
+        <Title title1={`Why`} title2={`choose us`} />
+      </div>
+      <div className="flex flex-col md:flex-row text-sm mb-20">
+        <div className="border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5">
+          <b>Quality Assurence :</b>
+          <p className="text-gray-60 text-justify">Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa at, fuga labore distinctio tempore iure amet mollitia neque error modi.</p>
+        </div>
+        <div className="border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5">
+          <b>Convenience :</b>
+          <p className="text-gray-60 text-justify">Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa at, fuga labore distinctio tempore iure amet mollitia neque error modi.</p>
+        </div>
+        <div className="border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5">
+          <b>Exceptional customer service :</b>
+          <p className="text-gray-60 text-justify">Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa at, fuga labore distinctio tempore iure amet mollitia neque error modi.</p>
+        </div>
+      </div>
+
+      <NewLetterBox/>
+    </div>
+  );
+};
+
+export default About;
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,9 +1,27 @@
 import React from 'react'
+import Title from '../components/Title'
+import {assets} from '../assets/assets'
+import NewLetterBox from '../components/NewLetterBox'
 
 const Contact = () => {
   return (
     <div>
-      Contact
+      <div className='text-center text-2xl pt-10 border-t'>
+        <Title title1={`contact`} title2={`us`} />
+      </div>
+      <div className="my10 flex flex-col justify-center md:flex-row gap-10 mb-28">
+        <img className='w-full md:max-w-[480px]' src={assets.contact_img} alt="Contact Image" />
+        <div className='flex flex-col justify-center items-start gap-6'>  
+          <p className='font-bold text-xl text-gray-600'>Our Store</p>
+          <p className='text-gray-500'>54709 Willms Stations <br/> Suite 350, Washigton, USA</p>
+          <p className='text-gray-500 '>Tel: (415) 555-0123 <br/> Email: example@gmail.com</p>
+          <p className='font-semibold text-xl text-gray-600'>Career at Forever</p>
+          <p className='text-gray-500'>Learn more about our teams and job openings</p>
+          <button className='border border-black px-8 py-4 text-sm hover:text-white transition-all duration-500'>Explore Jobs</button>
+        </div>
+      </div>
+      <NewLetterBox/>
+
     </div>
   )
 }
